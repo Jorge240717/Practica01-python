0918d4bccc5f421811aa93ee88743583be9dbb6a
Jorge240717 <157293562+Jorge240717@users.noreply.github.com>
2024-01-25 17:27:02 +0000

    problema3

---
+++ problema3.py
@@ -0,0 +1,6 @@
+peso_payaso = 112
+peso_muñeca = 75
+cantidad_payasos = int(input("Ingrese el número de payasos vendidos: "))
+cantidad_muñecas = int(input("Ingrese el número de muñecas vendidas: "))
+peso_total = (peso_payaso * cantidad_payasos) + (peso_muñeca * cantidad_muñecas)
+print(f"El peso total del paquete es {peso_total} gramos.")
